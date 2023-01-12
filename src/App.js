e1b4d78c1ee5768e4589752de9a704304a337812
Grigor Yankov <yankovweb@outlook.com>
2023-01-12 22:23:44 +0200

    [Future]useCallBack

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutPut from "./components/Demo/DemoOutPut";
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutPut show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
