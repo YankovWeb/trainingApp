484b02eb265129d5f643cab4111419c3da67343e
Grigor Yankov <yankovweb@outlook.com>
2023-01-12 22:06:59 +0200

    [Future]useMemo

--- src/components/Demo/DemoOutPut.js
+++ src/components/Demo/DemoOutPut.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MyParagraph from "./MyParagraph";
+import {memo} from "react";
 const DemoOutPut = (props) => {
   return <MyParagraph>{props.show ? "this is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutPut;
+export default memo(DemoOutPut);
